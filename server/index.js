15dddb314ae8c8a3174753515fba0b334ce293ef
Rahul <rahulrdb18@gmail.com>
2020-03-16 18:57:36 +0000

    first commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+
+// Middleware
+//
+
+app.use(bodyParser.json()
+
+
+
+
